d0d38ebc78a228fa3d980a27097bc64bab56e830
don Rumata <v0541k@yandex.ru>
2020-05-20 02:15:55 +0300

    Набыдлокодил исключение для foacl.

--- install-ansible-4-ubuntu.sh
+++ install-ansible-4-ubuntu.sh
@@ -3,7 +3,11 @@
 # 4 docker in https://circleci.com/gh/don-rumata.
 
 source /etc/os-release
-echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu $VERSION_CODENAME main" | sudo tee /etc/apt/sources.list.d/ansible.list
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
+
+	if	[ "$VERSION_CODENAME" != "focal" ] ; then
+		echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu $VERSION_CODENAME main" | sudo tee /etc/apt/sources.list.d/ansible.list ;
+        sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367 ;
+	fi
+
 sudo apt update
 sudo apt -y install ansible
